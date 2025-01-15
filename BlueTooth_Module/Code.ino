ebfd95bbaecc3b53a651fcb133e7536cb2ed8b3a
Angad Pal <Angad4548p@gmail.com>
2025-01-15 22:17:16 +0530

    Update Code.ino

--- BlueTooth_Module/Code.ino
+++ BlueTooth_Module/Code.ino
@@ -1,14 +1,18 @@
-char receivedData;  // Variable to store received data from Bluetooth
+char receivedData;  // Variable to store received data
 
 void setup() {
-  Serial.begin(9600);  // Start serial communication at 9600 baud rate
+  Serial.begin(9600);  // Start serial communication
   Serial.println("Bluetooth Communication Started...");
 }
 
 void loop() {
+  // Check if data is available from Bluetooth
   if (Serial.available() > 0) {
-    receivedData = Serial.read();  // Read incoming data from Bluetooth
-    Serial.print("Received Data: ");
-    Serial.println(receivedData);  // Print received data to the Serial Monitor
+    receivedData = Serial.read();  // Read received data from Bluetooth
+    
+    // If the received data is '1', print 'Hello'
+    if (receivedData == '1') {
+      Serial.println("Hello");  // Print 'Hello' to the Serial Monitor
+    }
   }
 }
