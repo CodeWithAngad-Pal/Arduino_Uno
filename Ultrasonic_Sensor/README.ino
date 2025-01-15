100a57f72ccf84e8fb110b5314976015a460ba8a
Angad Pal <Angad4548p@gmail.com>
2025-01-15 23:11:08 +0530

    Update README.ino

--- Ultrasonic_Sensor/README.ino
+++ Ultrasonic_Sensor/README.ino
@@ -10,9 +10,9 @@ This guide demonstrates how to use an ultrasonic sensor (e.g., **HC-SR04**) with
 
 # Wiring for HC-SR04 Ultrasonic Sensor
 
-| **HC-SR04 Pin** | **Arduino Pin**    | **Description**                      |
-|-----------------|--------------------|--------------------------------------|
-| VCC             | 5V                 | Power supply for the ultrasonic sensor|
-| GND             | GND                | Ground connection                   |
-| TRIG            | Pin 9 (or any digital pin) | Trigger pin to send pulse           |
-| ECHO            | Pin 10 (or any digital pin) | Echo pin to receive reflected pulse |
+| **HC-SR04 Pin** | **Arduino Pin**          | **Description**                       |
+|-----------------|--------------------------|---------------------------------------|
+| VCC             | 5V                       | Power supply for the ultrasonic sensor|
+| GND             | GND                      | Ground connection                    |
+| TRIG            | Pin 9 (or any digital pin)| Trigger pin to send pulse            |
+| ECHO            | Pin 10 (or any digital pin)| Echo pin to receive reflected pulse  |
